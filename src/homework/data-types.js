7804f0f29d2790ae65a6d5fc09935ad4107f72ca
Alexandr19961802 <besencevaleksandr4@gmail.com>
2026-03-02 13:50:54 +0200

    Add JS core concepts homework

---
+++ src/homework/data-types.js
@@ -0,0 +1,19 @@
+
+let a = 5;
+let b = a;
+
+b = 10;
+
+console.log("Primitive example:");
+console.log("a:", a); 
+console.log("b:", b); 
+
+
+
+let obj1 = { name: "Alex" };
+let obj2 = obj1;
+
+obj2.name = "John";
+
+console.log("\nObject example:");
+console.log("obj1.name:", obj1.name);
